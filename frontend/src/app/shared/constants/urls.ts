32767ae08f3dcf8b1794c6280f8bb649b9f4f948
yashwant <yashwantyadav71@gmail.com>
2024-01-19 23:16:28 +0530

    url

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,8 +1,9 @@
-import { environment } from "src/environments/environment";
+import { environment } from 'src/environments/environment';
 
-const BASE_URL = environment.production? '' : 'http://localhost:5000';
+const BASE_URL = environment.production
+  ? ''
+  : 'https://seatbookerassign.onrender.com';
 
 export const SEAT_GET_URL = BASE_URL + '/api/seats';
 export const SEAT_BOOK_URL = BASE_URL + '/api/seatbook';
 export const RESEED_URL = BASE_URL + '/api/reseed';
-
